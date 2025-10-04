e2a081032d03e07bca241b4ded4fe4595f7dc469
LE DOAN DAN <ledoandan2110@gmail.com>
2025-10-04 17:32:18 +0700

    BTTH_Tuan5_LeDoanDan

---
+++ Sach.java
@@ -0,0 +1,87 @@
+
+package sach;
+
+public class Sach 
+{
+    private String maSach;
+    private String tieuDe;
+    private String tacGia;
+    private int namXuatBan;
+    private int soLuong; 
+
+    public Sach(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong) 
+    {
+        this.maSach = maSach;
+        this.tieuDe = tieuDe;
+        this.tacGia = tacGia;
+        this.namXuatBan = namXuatBan;
+        this.soLuong = soLuong;
+    }
+    
+    public Sach(String maSach, String tacGia, int soLuong) 
+    {
+        this.maSach = maSach;
+        this.tacGia = tacGia;
+        this.soLuong = soLuong;
+    }
+
+    public Sach(String maSach, String tieuDe, String tacGia) 
+    {
+        this.maSach = maSach;
+        this.tieuDe = tieuDe;
+        this.tacGia = tacGia;
+    }
+    
+    
+    public String getmaSach()
+    {
+        return maSach;
+    }
+    public void setmaSach(String maSach)
+    {
+        this.maSach=maSach;
+    }
+    public String gettieuDe()
+    {
+        return tieuDe;
+    }
+    public void settieuDe(String tieuDe)
+    {
+        this.tieuDe=tieuDe;
+    }
+    public String gettacGia()
+    {
+        return tacGia;
+    }
+    public void settacGia(String tacGia)
+    {
+        this.tacGia=tacGia;
+    }
+    public int getnamXuatBan()
+    {
+        return namXuatBan;
+    }
+    public void setnamXuatBan(int namXuatBan)
+    {
+        this.namXuatBan=namXuatBan;
+    }
+    public int getsoLuong()
+    {
+        return soLuong;
+    }
+    public void setsoLuong(int soLuong)
+    {
+        this.soLuong=soLuong;
+    }
+    
+    public void hienThi() 
+    {
+        System.out.println("Ma sach: " + (maSach != null ? maSach : "--"));
+        System.out.println("Tieu de: " + (tieuDe != null ? tieuDe : "--"));
+        System.out.println("Tac gia: " + (tacGia != null ? tacGia : "--"));
+        System.out.println("Nam xuat ban: " + (namXuatBan != 0 ? namXuatBan : "--"));
+        System.out.println("So luong: " + (soLuong != 0 ? soLuong : "--"));
+    }
+
+    
+}
---
+++ Test.java
@@ -0,0 +1,17 @@
+
+package sach;
+
+public class Test {
+    public static void main(String[] args)
+    {
+       Sach s1=new Sach("sach1", "Giao khoa","Nguyen Van A", 2024,10);
+       s1.hienThi();
+       System.out.println("");
+       Sach s2=new Sach("sach2", "Nguyen Van B",20);
+       s2.hienThi();
+       System.out.println("");
+       Sach s3=new Sach("sach2", "Truyen tranh","Nguyen Van B");
+       s3.hienThi();
+    }
+    
+}
