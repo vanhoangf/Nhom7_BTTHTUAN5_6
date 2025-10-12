446a3a17a34e1c58c5d0992cca3212ef8e625561
Lê Đình Danh <danhledinh2607@gmail.com>
2025-10-12 20:03:19 +0700

    BTTHTUAN6_LeDinhDanh

---
+++ Tuan6/Sach.java
@@ -0,0 +1,79 @@
+package BTTUAN5;
+
+public class Sach
+{
+    protected String maSach;
+    protected String tenSach;
+    protected String tacGia;
+    protected int namXuatBan;
+    protected int soLuong;
+
+    public Sach()
+    {
+    }
+
+    public Sach(String maSach, String tenSach, String tacGia, int namXuatBan, int soLuong)
+    {
+        this.maSach = maSach;
+        this.tenSach = tenSach;
+        this.tacGia = tacGia;
+        this.namXuatBan = namXuatBan;
+        this.soLuong = soLuong;
+    }
+
+
+    public void hienThiThongTin()
+    {
+        System.out.println("Mã sách: " + maSach);
+        System.out.println("Tên sách: " + tenSach);
+        System.out.println("Tác giả: " + tacGia);
+        System.out.println("Năm xuất bản: " + namXuatBan);
+        System.out.println("Số lượng: " + soLuong);
+    }
+
+    // setter trước rồi getter sau
+    public void setMaSach(String maSach)
+    {
+        this.maSach = maSach;
+    }
+    public String getMaSach()
+    {
+        return maSach;
+    }
+
+    public void setTenSach(String tenSach)
+    {
+        this.tenSach = tenSach;
+    }
+    public String getTenSach()
+    {
+        return tenSach;
+    }
+
+    public void setTacGia(String tacGia)
+    {
+        this.tacGia = tacGia;
+    }
+    public String getTacGia()
+    {
+        return tacGia;
+    }
+
+    public void setNamXuatBan(int namXuatBan)
+    {
+        this.namXuatBan = namXuatBan;
+    }
+    public int getNamXuatBan()
+    {
+        return namXuatBan;
+    }
+
+    public void setSoLuong(int soLuong)
+    {
+        this.soLuong = soLuong;
+    }
+    public int getSoLuong()
+    {
+        return soLuong;
+    }
+}
---
+++ Tuan6/Test.java
@@ -0,0 +1,29 @@
+package BTTUAN5;
+import java.util.Scanner;
+public class Test
+{
+    public static void main(String[] args)
+    {
+       Scanner sc = new Scanner(System.in);
+       QuanLiSach qls = new QuanLiSach();
+       qls.themSach(new SachTieuThuyet("S001", "Harry Potter", "J.K.Rowling", 2005, 10, "Võng Sư", true));
+       qls.themSach(new SachGiaoTrinh("S003", "Lập Trình Java", "Nguyễn Nguyệt Nga", 2015, 20, "Lập Trình", "Cao"));
+       qls.hienThiDanhSach();
+       System.out.print("Nhập mã sách muốn tìm: ");
+       String masach = sc.nextLine();
+       Sach timsach = qls.timKiemSach(masach);
+       if (timsach != null)
+            timsach.hienThiThongTin();
+       else
+            System.out.println("Không tìm thấy sách.");
+       System.out.println("Nhập thông tin mã sách muốn cập nhật: ");
+       String macnn = sc.nextLine();
+       qls.capNhatSach(macnn, new SachTieuThuyet("S001", "ROnaldinho", "J.K.Rowling", 2005, 10, "Võng Sư", true));
+       qls.hienThiDanhSach();
+       System.out.println("Nhập thông tin sách muốn xóa: ");
+       String mxoa = sc.nextLine();
+       qls.xoaSach(mxoa);
+       qls.hienThiDanhSach();
+    }
+
+}
